c56c134ee108f66710119143301117d7f11b759e
Artur Komaristych <artur.komaristych@infolytics.com>
2023-02-08 20:17:21 +0100

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  extends: "airbnb-typescript/base",
+  plugins: ["import", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+};
